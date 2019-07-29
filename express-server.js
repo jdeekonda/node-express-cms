5e497fe1f08c93d99a56b7250eb7141a46f534a0
Jagadeesh Deekonda <jdeekonda@gmail.com>
2019-07-29 23:54:35 +0200

    feat:send request to node server

--- express-server.js
+++ express-server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req,res){
-  res.send('Hello from express server');
+  // console.log(req);
+  res.send('This is home page');
+});
+
+app.post('/', function(req,res){
+  // console.log(req.body.filename);
+  // console.log(req.body.content);
+  res.send('Post request received for '+req.body.filename+' and '+ req.body.content);
 });
 
 app.listen(7000, function(){
